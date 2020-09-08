20a6bd32c313cf90613e4a3abdb53c6b89148a1b
Abjec <maxime.radomski@gmail.com>
2020-09-08 22:49:37 +0200

    double jump... yeah...

--- Assets/Scripts/Models/Piece.cs
+++ Assets/Scripts/Models/Piece.cs
@@ -12,6 +12,10 @@ public class Piece : MonoBehaviour
     public bool HasBlocksAffectedByGravity;
     public RotationState RotationState = RotationState.O;
 
+    private Vector3 _originalScale;
+    private Vector3 _doubleJumpScale = new Vector3(1.3f, 1.3f, 1.0f);
+    private bool _isDoubleJumping = false;
+
     public void HandleOpacityOnLock(float percent)
     {
         foreach (Transform child in transform)
@@ -27,6 +31,31 @@ public class Piece : MonoBehaviour
         {
             var tmpColor = child.gameObject.GetComponent<SpriteRenderer>().color;
             child.gameObject.GetComponent<SpriteRenderer>().color = color;
+        }
+    }
+
+    private void Update()
+    {
+        if (_isDoubleJumping)
+        {
+            DoubleJumping();
+        }
+    }
+
+    public void DoubleJump()
+    {
+        _originalScale = transform.localScale;
+        transform.localScale = _doubleJumpScale;
+        _isDoubleJumping = true;
+    }
+
+    private void DoubleJumping()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, _originalScale, 0.2f);
+        if (Helper.VectorEqualsPrecision(transform.localScale, _originalScale, 0.01f))
+        {
+            transform.localScale = _originalScale;
+            _isDoubleJumping = false;
         }
     }
 }
