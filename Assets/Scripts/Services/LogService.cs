373c6e7eb19efbf8d04750155d77c28223a87894
Abject <maxime.radomski@gmail.com>
2022-01-22 23:16:33 +0100

    Better Logs

--- Assets/Scripts/Services/LogService.cs
+++ Assets/Scripts/Services/LogService.cs
@@ -30,9 +30,22 @@ public static class LogService
                 _currentSessionLogs = $"----------------------------\nSession: {Helper.DateFormat(DateTime.Now)}\n----------------------------\n";
                 File.AppendAllText(path, _currentSessionLogs);
             }
-            var tmpLog = $"[{type.ToString().ToUpper()} - {SceneManager.GetActiveScene().name}] {condition}\n\t{stackTrace.Replace("\n", "\n\t")}";
-            if (tmpLog[tmpLog.Length - 1] == '\t')
-                tmpLog = tmpLog.Substring(0, tmpLog.Length - 1);
+
+            var context = "";
+            var currentScene = GameObject.Find(Constants.GoSceneBhvName).GetComponent<SceneBhv>();
+            if (currentScene is ClassicGameSceneBhv gameScene)
+            {
+                var gameplayControler = GameObject.Find(Constants.GoSceneBhvName).GetComponent<GameplayControler>();
+                context = $"\n    [CONTEXT] Opponent: {gameScene.CurrentOpponent?.Name}, Character: {gameScene.Character?.Name}, Item: {gameplayControler?.CharacterItem?.Name}, CacheOpponentAttackId: {Cache.CurrentOpponentAttackId}";
+            }
+            else if (currentScene is LoreSceneBhv loreScene)
+            {
+                context = $"\n    [CONTEXT] NavigationService.SceneParameter: {JsonUtility.ToJson(NavigationService.SceneParameter)}";
+            }
+
+            var tmpLog = $"[{type.ToString().ToUpper()} - {SceneManager.GetActiveScene().name}] {condition}{context}\n    {stackTrace.Replace("\n", "\n    ")}";
+            if (tmpLog.Substring(tmpLog.Length - 4) == "    ")
+                tmpLog = tmpLog.Substring(0, tmpLog.Length - 4);
             _currentSessionLogs = tmpLog;
             File.AppendAllText(path, _currentSessionLogs);
         }
